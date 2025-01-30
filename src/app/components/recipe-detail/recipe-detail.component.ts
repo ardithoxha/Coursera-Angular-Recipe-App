1944de2c7ebbabfb03c68cd198162d0c92264015
Ardit <ardit.hoxha@gmail.com>
2025-01-30 13:11:22 -0500

    udpated recipe component to show recipe details

--- src/app/components/recipe-detail/recipe-detail.component.ts
+++ src/app/components/recipe-detail/recipe-detail.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Recipe } from '../../models/recipe.model';
+import { RecipeService } from '../../services/recipe.service';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -7,6 +10,14 @@ import { Component } from '@angular/core';
   styleUrl: './recipe-detail.component.css',
   standalone: true,
 })
-export class RecipeDetailComponent {
+export class RecipeDetailComponent implements OnInit {
+  recipe?: Recipe;
+
+  constructor(private route: ActivatedRoute, private recipeService: RecipeService) {}
+
+  ngOnInit() {
+    const id = +this.route.snapshot.params.id;
+    this.recipe = this.recipeService.getRecipeById(id);
+  }
 
 }
