a3c78f98e838cbaf6a368ae4a321329a0f6c2509
Ardit <ardit.hoxha@gmail.com>
2025-01-30 13:10:11 -0500

    updated recipe list view

--- src/app/components/recipe-list/recipe-list.component.ts
+++ src/app/components/recipe-list/recipe-list.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { RecipeService } from '../../services/recipe.service';
 import { Recipe } from '../../models/recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './recipe-list.component.html',
   styleUrl: './recipe-list.component.css',
   standalone: true,
