14d0e60c9de3fb0d52a01ef5c6b472651fa56155
Ardit <ardit.hoxha@gmail.com>
2025-01-30 13:12:37 -0500

    added new form view to add new recipe along with the missing route paths. Also added a sandwith picture for demo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { RecipeListComponent } from './components/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './components/recipe-detail/recipe-detail.component';
+import { RecipeFormComponent } from './components/recipe-form/recipe-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '', component: RecipeListComponent
+    },
+    {
+        path: 'recipe/:id', component: RecipeDetailComponent
+    },
+    {
+        path: 'add-recipe', component: RecipeFormComponent
+    },
+];
--- src/app/components/recipe-form/recipe-form.component.ts
+++ src/app/components/recipe-form/recipe-form.component.ts
@@ -1,12 +1,53 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../../models/recipe.model';
+import { RecipeService } from '../../services/recipe.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-recipe-form',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './recipe-form.component.html',
   styleUrl: './recipe-form.component.css',
   standalone: true,
 })
 export class RecipeFormComponent {
+  newRecipe: Recipe = {
+    id: 0,
+    name: '',
+    ingredients: [],
+    instructions: '',
+    imageUrl: ''
+  };
+  ingredientInput: string = '';
+  
+  constructor(private recipeService: RecipeService) {}
+  
+  addIngredient() {
+    if (this.ingredientInput.trim()) {
+      this.newRecipe.ingredients.push(this.ingredientInput.trim());
+      this.ingredientInput= '';
+    }
+  }
 
+  removeIngredient(index: number) {
+    this.newRecipe.ingredients.splice(index, 1);
+  }
+
+  submitRecipe() {
+    // the below if statement is for an easy demo of adding a sandwitch recipe
+    // otherwise the user has to provide a full url of the image for it to work
+    if (this.newRecipe.imageUrl === 'sandwitch') {
+      this.newRecipe.imageUrl = '../../assets/sandwitch.jpg'
+    }
+    if (this.newRecipe.name && this.newRecipe.instructions) {
+      this.recipeService.addRecipe(this.newRecipe);
+      this.newRecipe = {
+        id: 0,
+        name: '',
+        ingredients: [],
+        instructions: '',
+        imageUrl: ''
+      };
+    }
+  }
 }
