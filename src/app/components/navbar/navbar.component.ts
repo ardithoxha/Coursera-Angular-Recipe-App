e8fbf011d4cb9d6e673a9b4b43581db9453d47f5
Ardit <ardit.hoxha@gmail.com>
2025-01-30 13:08:38 -0500

    added navbar details

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
   standalone: true,
 })
 export class NavbarComponent {
-  
+
 }
