5a94b07a9f928db4d0b737cedce32db6cd3db3a9
Ardit <ardit.hoxha@gmail.com>
2025-01-30 18:12:05 -0500

    improved styling for recipe list and added mobile view

--- src/app/components/recipe-list/recipe-list.component.ts
+++ src/app/components/recipe-list/recipe-list.component.ts
@@ -1,18 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { NgStyle } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { RecipeService } from '../../services/recipe.service';
 import { Recipe } from '../../models/recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
-  imports: [RouterLink],
+  imports: [RouterLink, NgStyle],
   templateUrl: './recipe-list.component.html',
   styleUrl: './recipe-list.component.css',
   standalone: true,
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [];
+
   constructor(private recipeService: RecipeService) {}
+
   ngOnInit(): void {
     this.recipes = this.recipeService.getRecipes();
   }
